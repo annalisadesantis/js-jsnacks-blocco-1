faec850f150d859da363102413916b0b04470889
Annalisa <annalisadesantis770@gmail.com>
2020-10-15 15:52:57 +0200

    try

---
+++ js-jsnack3/main.js
@@ -0,0 +1,20 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un
+// numero.
+
+var numero_utente = parseInt(prompt("Inserisci il primo numero"));
+
+var numero_utente = parseInt(prompt("Inserisci il secondo numero"));
+
+var numero_utente = parseInt(prompt("Inserisci il terzo numero"));
+
+var numero_utente = parseInt(prompt("Inserisci il quarto numero"));
+
+var numero_utente = parseInt(prompt("Inserisci il quinto numero"));
+
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+for (var i = 0; i < numero_utente; i++){
+
+    var somma = (0 + numero_utente);
+}
